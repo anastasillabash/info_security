61e5ebec988458948f2e9cae4212c551da4c28dc
Anastasiia Bash <anastasiill@gmail.com>
2022-12-28 00:11:38 +0200

    pr_1

---
+++ practice 1/practice 1/1.2/practice1.2.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+
+class Random_Crypt_Generator
+{
+    public static void Main()
+    {
+        Console.Write("Enter lenght: ");
+        int length = Convert.ToInt32(Console.ReadLine());
+
+        var rndNumberGenerator = new RNGCryptoServiceProvider();
+        var rndNum = new byte[length];
+
+        rndNumberGenerator.GetBytes(rndNum);
+        string cryptoKey = Convert.ToBase64String(rndNum);
+
+        Console.Write("\nBytes: ");
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write(rndNum[i]);
+        };
+
+        Console.Write("\n\nText: ");
+        Console.WriteLine(cryptoKey);
+    }
+}
---
+++ practice 1/practice 1/practice 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Random_Generator
+{
+    public static void Main()
+    {
+        Console.WriteLine("The amount of numbers: ");
+        int amount = Convert.ToInt32(Console.ReadLine()); //кількість чисел для генерації
+
+        Console.WriteLine("Minimum of range: ");
+        int min = Convert.ToInt32(Console.ReadLine()); //мінімум
+
+        Console.WriteLine("Maximum of range: ");
+        int max = Convert.ToInt32(Console.ReadLine()); //максимум
+
+        Console.WriteLine(" ");
+
+        if (max > min) //перевірка на правильність вводу чисел
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < amount; i++)
+            {
+                Console.WriteLine(rnd.Next(min, max) + " ");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Wrong minimum/maximum");
+        }
+        Console.ReadLine();
+    }
+
+};
